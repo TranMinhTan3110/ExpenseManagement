1f31de060df418cf0d5152ca382dfc07ed405c6b
AuDuongTan <auduongtan321@gmail.com>
2025-10-16 10:10:03 +0700

    Add login page and ignore build files

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyChiTieu_WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Reset()
+        {
+            return View();
+        }
+    }
+}
