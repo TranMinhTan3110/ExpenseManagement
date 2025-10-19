4272ce07d79fb1d9f5663bb34e19df58855c1128
AuDuongTan <auduongtan321@gmail.com>
2025-10-19 15:17:37 +0700

    complete initial UI settings profile

---
+++ Controllers/SettingsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace QuanLyChiTieu_WebApp.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
