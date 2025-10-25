040663a48f2f3fe0b051368f7049655f649de561
BinHG05 <Nguyensubins1@gmail.com>
2025-10-25 20:44:39 +0700

    Update Goals

--- ExpenseManagement/Controllers/HomeController.cs
+++ ExpenseManagement/Controllers/HomeController.cs
@@ -23,10 +23,6 @@ namespace QuanLyChiTieu_WebApp.Controllers
             return View();
         }
 
-        public IActionResult Goals()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- ExpenseManagement/obj/Release/net8.0/QuanLyChiTieu_WebApp.AssemblyInfo.cs
+++ ExpenseManagement/obj/Release/net8.0/QuanLyChiTieu_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyChiTieu_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6040997dc5667a0b36af5b6ea604223770afd068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89c8c6cabde05de194ed4eb95812b329c917e6dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyChiTieu_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyChiTieu_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
