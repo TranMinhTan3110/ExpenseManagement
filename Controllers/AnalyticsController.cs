5c8ef3b516de952390f8e52e9caca1515a22f29a
AuDuongTan <auduongtan321@gmail.com>
2025-10-26 23:29:43 +0700

    Creating Analytics page

---
+++ Controllers/AnalyticsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyChiTieu_WebApp.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        public IActionResult Income()
+        {
+            return View();
+        }
+        public IActionResult Expense()
+        {
+            return View();
+        }
+    }
+}
